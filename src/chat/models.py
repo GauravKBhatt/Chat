87bb9d7785debe72c512f747115f5e5d0bd65c44
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-05-05 14:55:31 +0545

    figured the manager and user model

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,3 +1,59 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.db import models
+from django.utils import timezone
+
+# UserManager is a Django class that provides helper methods for creating and managing user objects in the database. 
+
+class CustomUserManager(UserManager):
+    # private method. Any method containing _ in front is a private method in django and it is not supposed to be called directly. 
+    # it contains the core logic of building the email and the public method create_user calls it. 
+    def _create_user(self, name, email, password, **extra_fields):
+        if not email:
+            raise ValueError("You have not provided a valid e-mail address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    # public methods. only sets the default values and then calls the private method.
+    def create_user(self, name=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(name, email, password, **extra_fields)
+    
+    def create_superuser(self, name=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(name, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    AGENT = 'agent'
+    MANAGER = 'manager'
+
+    ROLES_CHOICES = (
+        (AGENT, 'Agent'),
+        (MANAGER, 'Manager'),
+    )
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True, default='')
+    role = models.CharField(max_length=20, choices=ROLES_CHOICES, default=AGENT)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
