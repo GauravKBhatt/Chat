59b5c0d59b1710c6dbf7243ad3a654716972be33
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-05-09 12:13:50 +0545

    Figured the URLS i guess

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),
-   ]
+    path('create-room/<path:url>/<uuid:uuid>/<str:name>/', Room.as_view(), name='create-room'),
+    path('chat-admin/<str:uuid>/',Room.as_view(),name='room'),
+    path('chat-admin/',Admin.as_view(),name='admin'),
+    path('chat-admin/add-user/<str:status>/',User.as_view(),name='add_edit_user'),
+    path('chat-admin/users/<uuid:uuid>/',User.as_view(),name='user_detail'),
+    # path('chat-admin/users/<uuid:uuid>/edit/',views.edit_user,name='edit_user'),
+    path('chat-admin/delete-room/<str:uuid>/',Delete.as_view(),name='delete_room')]
